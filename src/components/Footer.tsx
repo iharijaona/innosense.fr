6716814dce723929a64224b3209535461b92693c
Harijaona Ravelondrina <hravelondrina@gmail.com>
2024-05-09 23:56:50 +0200

    Page footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Footer = () => {
   return (
-    <footer className="mx-auto max-w-7xl px-6 lg:px-8 mt-24 w-full sm:mt-32 lg:mt-40" >
+    <footer className="mx-auto max-w-7xl px-6 lg:px-8 mt-24  w-full sm:mt-32 lg:mt-40" >
       <div className="mx-auto max-w-2xl lg:max-w-none" >
         <div className="mb-20 mt-24 flex flex-col items-center sm:flex-wrap sm:flex-row sm:items-end sm:justify-between gap-x-6 gap-y-4 border-t border-neutral-950/10 pt-12">
           <p className="text-sm text-neutral-700">Copyright © {new Date().getFullYear()} | Tous droits réservés.</p>
